6a2cc57ec22cb8209e8743a92d10e1ca6bb58e09
MarecoAJ <alejo_mareco@hotmail.com>
2023-05-17 18:13:36 -0300

    arreglo nombre de tabla

--- 01-table-marcaVehiculo.sql
+++ 01-table-marcaVehiculo.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `db_eldoradoparabrisas`.`marcaVehiculos` (
+CREATE TABLE `db_eldoradoparabrisas`.`marcaVehiculo` (
 `codMarcaVeh` INT(15) PRIMARY KEY NOT NULL,
 `descrMarcaVeh` VARCHAR(45) NULL,
 `fechaInsercion` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
