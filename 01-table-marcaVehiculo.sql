4a0b557f47d77b6018e7cb935db30e415c32bbdf
MarecoAJ <alejo_mareco@hotmail.com>
2023-05-17 18:36:55 -0300

    arreglo tipo attr de tabla

--- 01-table-marcaVehiculo.sql
+++ 01-table-marcaVehiculo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `db_eldoradoparabrisas`.`marcaVehiculo` (
-`codMarcaVeh` INT(15) PRIMARY KEY NOT NULL,
+`codMarcaVeh` VARCHAR(15) PRIMARY KEY NOT NULL,
 `descrMarcaVeh` VARCHAR(45) NULL,
 `fechaInsercion` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 `estadoEliminacion` INT NULL DEFAULT 1
